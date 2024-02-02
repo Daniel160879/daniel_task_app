d57bcc66b8dab44212cc886b944c6733558ed3e2
Daniel <danielhandel4@gmail.com>
2024-02-02 12:53:20 +0100

    aufgaben dran,test

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -2,8 +2,29 @@ import 'package:flutter/material.dart';
 
 class S4522 extends StatelessWidget {
   const S4522({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MaterialApp(title: 'MyStatelessWidget', home: MyStatelessWidget(S4522));
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget(Type s4522, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text("Hello Daniel"),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Klick mich"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
-class S4523 extends StatelessWidget {
-  const S4523({Key? key}) : super(key: key);
+class S4523 extends StatefulWidget {
+  const S4523({super.key});
+
+  @override
+  State<S4523> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<S4523> {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text(''),
+            ElevatedButton(
+              onPressed: () {
+                Text('$State');
+              },
+              child: const Text('Daniel'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
